978316474c3c103e49c9a5e1b6e4686f24cffcd5
guiac <gui.acassemiro@gmail.com>
2022-11-29 22:43:31 -0300

    :recycle: refactor signup

--- src/domain/usecases/signup.ts
+++ src/domain/usecases/signup.ts
@@ -1,5 +1,21 @@
-import { SignUpModel } from '../models'
-
 export interface SignUp {
-    handle: (data: SignUpModel) => Promise<void>
+    handle: (data: SignUp.Request) => Promise<SignUp.Result>
+}
+
+export namespace SignUp {
+    export type Request = {
+        email: string
+        password: string
+        name: string
+        lastName: string
+        birthDate: Date
+        tellphone: string
+        cellphone: string
+        streetAddress: string
+        numberAddress: string
+        districtAddress: string
+        cityAddress: string
+        stateAddress: string
+    }
+    export type Result = boolean
 }
--- tests/presentation/mocks/mock.signup.ts
+++ tests/presentation/mocks/mock.signup.ts
@@ -2,7 +2,7 @@ import { SignUp } from '@/domain/usecases'
 
 export class SignUpSpy implements SignUp {
     input: any
-    async handle(input: any): Promise<void> {
+    async handle(input: SignUp.Request): Promise<SignUp.Result> {
         this.input = input
         return this.input
     }
