19de0815da1109179c659f9b646e7defe477b527
guiac <gui.acassemiro@gmail.com>
2022-11-29 22:55:19 -0300

    :test_tube: ensure SignUpController calls Authentication with correct values

--- tests/presentation/controllers/signup.controller.spec.ts
+++ tests/presentation/controllers/signup.controller.spec.ts
@@ -1,5 +1,5 @@
 import { SignupController } from '@/presentation/controllers'
-import { ValidationSpy, SignUpSpy } from '../mocks'
+import { ValidationSpy, SignUpSpy, AuthSpy } from '../mocks'
 import { serverError } from '@/presentation/helpers/http-helper'
 
 const mockRequest = (): SignupController.Request => ({
@@ -25,15 +25,18 @@ type SutTypes = {
     sut: SignupController
     validationSpy: ValidationSpy
     signUpSpy: SignUpSpy
+    authSpy: AuthSpy
 }
 
 const makeSut = (): SutTypes => {
     const validationSpy = new ValidationSpy()
     const signUpSpy = new SignUpSpy()
-    const sut = new SignupController(validationSpy, signUpSpy)
+    const authSpy = new AuthSpy()
+    const sut = new SignupController(validationSpy, signUpSpy, authSpy)
     return {
         validationSpy,
         signUpSpy,
+        authSpy,
         sut
     }
 }
@@ -66,5 +69,15 @@ describe('SignupController', () => {
         const request = mockRequest()
         const response = await sut.handle(request)
         expect(response).toEqual(serverError(new Error()))
+    })
+
+    test('Should call Authentication with correct values', async () => {
+        const { sut, authSpy } = makeSut()
+        const request = mockRequest()
+        await sut.handle(request)
+        expect(authSpy.input).toEqual({
+            email: request.email,
+            password: request.password
+        })
     })
 })
