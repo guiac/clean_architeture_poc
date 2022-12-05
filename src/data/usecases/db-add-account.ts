213bc007dee5a99dcb961f361863bade3176062f
guiac <gui.acassemiro@gmail.com>
2022-12-04 21:24:53 -0300

    :sparkles: ensure AddAccountRepository is called with correct values

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -5,14 +5,18 @@ import { AddAccountRepository, AuthenticationRepository, CheckAccountByEmailRepo
 export class DbAddAccount implements AddAccount {
     constructor(
         private readonly hasher: Hasher,
-        private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository
-        // private readonly AddAccountRepository: AddAccountRepository
+        private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository,
+        private readonly addAccountRepository: AddAccountRepository
         // private readonly authenticationRepository: AuthenticationRepository
     ) { }
 
     async handle(data: AddAccount.Request): Promise<any> {
-        await this.checkAccountByEmailRepository.checkAccountByEmail(data.email)
-        const hashedPassword = await this.hasher.hash(data.password)
-        return hashedPassword
+        const account = await this.checkAccountByEmailRepository.checkAccountByEmail(data.email)
+        if (!account) {
+            const hashedPassword = await this.hasher.hash(data.password)
+            const account = await this.addAccountRepository.save({ ...data, password: hashedPassword })
+            return account
+        }
+        return true
     }
 }
--- tests/data/mocks/mock.db.add.account.ts
+++ tests/data/mocks/mock.db.add.account.ts
@@ -3,7 +3,7 @@ import { Hasher } from '@/data/protocols/cryptography'
 
 export class CheckAccountByEmailRepositorySpy implements CheckAccountByEmailRepository {
     params: any
-    result: any = true
+    result: any = false
     async checkAccountByEmail(email: string): Promise<AddAccountRepository.Result> {
         this.params = email
         return this.result
