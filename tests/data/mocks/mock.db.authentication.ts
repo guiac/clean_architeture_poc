dacefa0e95ef74f54bb31026cc1318e45cc729a5
guiac <gui.acassemiro@gmail.com>
2022-12-05 21:53:01 -0300

    :recycle: moving LoadAccountByEmailRepositorySpy to mock.db.authentication

--- tests/data/mocks/mock.db.authentication.ts
+++ tests/data/mocks/mock.db.authentication.ts
@@ -1,9 +1,18 @@
-import { AuthenticationRepository } from '@/data/protocols/db'
+import { AuthenticationRepository, LoadAccountByEmailRepository } from '@/data/protocols/db'
 
 export class AuthenticationRepositorySpy implements AuthenticationRepository {
     params: any
     result: any = true
     async authentication(params: AuthenticationRepository.Params): Promise<AuthenticationRepository.Result> {
+        this.params = params
+        return this.result
+    }
+}
+
+export class LoadAccountByEmailRepositorySpy implements LoadAccountByEmailRepository {
+    params: any
+    result: any = true
+    async loadAccountByEmail(params: any): Promise<LoadAccountByEmailRepository.Result> {
         this.params = params
         return this.result
     }
