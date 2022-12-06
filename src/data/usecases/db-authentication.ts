44bd2686d54de3e595a4b214dcb145331a39359a
guiac <gui.acassemiro@gmail.com>
2022-12-05 22:17:29 -0300

    :sparkles: ensure HasheComparer is called with correct value

--- src/data/usecases/db-authentication.ts
+++ src/data/usecases/db-authentication.ts
@@ -1,13 +1,18 @@
 import { Authentication } from '@/domain/usecases'
 import { LoadAccountByEmailRepository } from '@/data/protocols/db'
+import { HashComparer } from '@/data/protocols/cryptography'
 
 export class DbAuthentication implements Authentication {
     constructor(
-        private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+        private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+        private readonly hashComparer: HashComparer
     ) { }
 
     async handle(data: Authentication.Request): Promise<any> {
-        await this.loadAccountByEmailRepository.loadAccountByEmail(data.email)
+        const account = await this.loadAccountByEmailRepository.loadAccountByEmail(data.email)
+        if (account) {
+            await this.hashComparer.compare(account.password)
+        }
         return null
     }
 }
--- tests/data/mocks/mock.db.authentication.ts
+++ tests/data/mocks/mock.db.authentication.ts
@@ -1,4 +1,5 @@
 import { AuthenticationRepository, LoadAccountByEmailRepository } from '@/data/protocols/db'
+import { HashComparer } from '@/data/protocols/cryptography'
 
 export class AuthenticationRepositorySpy implements AuthenticationRepository {
     params: any
@@ -11,9 +12,32 @@ export class AuthenticationRepositorySpy implements AuthenticationRepository {
 
 export class LoadAccountByEmailRepositorySpy implements LoadAccountByEmailRepository {
     params: any
-    result: any = true
+    result: any = {
+        email: 'email',
+        password: 'password',
+        name: 'name',
+        lastName: 'lastName',
+        birthDate: new Date(),
+        tellphone: 'tellphone',
+        cellphone: 'cellphone',
+        streetAddress: 'streetAddress',
+        numberAddress: 'numberAddress',
+        districtAddress: 'districtAddress',
+        cityAddress: 'cityAddress',
+        stateAddress: 'stateAddress'
+    }
+
     async loadAccountByEmail(params: any): Promise<LoadAccountByEmailRepository.Result> {
         this.params = params
+        return this.result
+    }
+}
+
+export class HashComparerSpy implements HashComparer {
+    hash: any
+    result: any = true
+    async compare(hash: string): Promise<boolean> {
+        this.hash = hash
         return this.result
     }
 }
