cea0729cdeb7b1d66375629396f3bc877e1d13a4
guiac <gui.acassemiro@gmail.com>
2022-12-07 21:30:01 -0300

    :test_tube: ensure EmailValidator is called with correct values

--- tests/validation/validators/email-validation.spec.ts
+++ tests/validation/validators/email-validation.spec.ts
@@ -3,8 +3,6 @@ import { EmailValidation } from '@/validation/validators'
 import { InvalidParamError } from '@/presentation/errors'
 import { EmailValidator } from '@/validation/protocols'
 
-const field = faker.random.word()
-const fiedlInput = faker.random.word()
 const email = faker.internet.email()
 
 interface SutTypes {
@@ -14,15 +12,15 @@ interface SutTypes {
 class EmailValidatorSpy implements EmailValidator {
   param: string
   isEmailValid: boolean = true
-  isValid(param: string): boolean {
-    this.param = param
+  isValid(email: string): boolean {
+    this.param = email
     return this.isEmailValid
   }
 }
 
 const makeSut = (): SutTypes => {
   const emailValidatorSpy = new EmailValidatorSpy()
-  const sut = new EmailValidation(field, emailValidatorSpy)
+  const sut = new EmailValidation('email', emailValidatorSpy)
   return {
     emailValidatorSpy,
     sut
@@ -33,7 +31,14 @@ describe('EmailValidation Validation', () => {
   test('Should return a InvalidParamError if validation fails', () => {
     const { sut, emailValidatorSpy } = makeSut()
     emailValidatorSpy.isEmailValid = false
-    const error = sut.validate({ [fiedlInput]: email })
-    expect(error).toEqual(new InvalidParamError(field))
+    const error = sut.validate({ email })
+    expect(error).toEqual(new InvalidParamError('email'))
+  })
+
+  test('Should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    sut.validate({ email })
+    console.log('emailValidatorSpy.param', emailValidatorSpy.param)
+    expect(emailValidatorSpy.param).toBe(email)
   })
 })
