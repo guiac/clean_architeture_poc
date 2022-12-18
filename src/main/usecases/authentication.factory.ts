8e2ec349e96b4c7c8ab3497570da25a94440be55
guiac <gui.acassemiro@gmail.com>
2022-12-18 20:09:34 -0300

    sparkles: creating makeDbAuthentication factory

---
+++ src/main/usecases/authentication.factory.ts
@@ -0,0 +1,13 @@
+import env from '@/main/config/env'
+import { DbAuthentication } from '@/data/usecases'
+import { BCryptAdapter, JwtAdapter } from '@/infra/cryptography'
+import { AccountMongoRepository } from '@/infra/db/mongodb'
+import { Authentication } from '@/domain/usecases'
+
+export const makeDbAuthentication = (): Authentication => {
+    const salt = 12
+    const accountMongoRepository = new AccountMongoRepository()
+    const bcryptAdapter = new BCryptAdapter(salt)
+    const jwtAdapter = new JwtAdapter(env.jwtSecret)
+    return new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter)
+}
