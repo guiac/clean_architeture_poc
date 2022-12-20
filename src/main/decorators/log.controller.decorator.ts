34ffce5fbc653f8ac99ea051c3efb31af4ff32db
guiac <gui.acassemiro@gmail.com>
2022-12-19 22:09:07 -0300

    :sparkles: ensure Controller is called with correct values

---
+++ src/main/decorators/log.controller.decorator.ts
@@ -0,0 +1,16 @@
+import { Controller, HttpResponse } from '@/presentation/protocols'
+
+export class LogControllerDecorator implements Controller {
+    constructor(
+        private readonly controller: Controller
+        // private readonly logRepository: LogRepository
+    ) { }
+
+    async handle(request: any): Promise<HttpResponse> {
+        const httpResponse = await this.controller.handle(request)
+        // if (httpResponse.statusCode === 500) {
+        //     await this.logRepository.logError(httpResponse.body)
+        // }
+        return httpResponse
+    }
+}
