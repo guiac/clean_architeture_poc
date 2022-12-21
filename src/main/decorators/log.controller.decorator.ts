06f0c79ee5dbaaf68cf53315af3b54958416fb4f
guiac <gui.acassemiro@gmail.com>
2022-12-21 07:21:46 -0300

    :sparkles: ensure LogErrorRepository is called with correct error if controller returns a server error

--- src/main/decorators/log.controller.decorator.ts
+++ src/main/decorators/log.controller.decorator.ts
@@ -1,16 +1,17 @@
+import { LogRepository } from '@/data/protocols/db/log'
 import { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class LogControllerDecorator implements Controller {
     constructor(
-        private readonly controller: Controller
-        // private readonly logRepository: LogRepository
+        private readonly controller: Controller,
+        private readonly logRepository: LogRepository
     ) { }
 
     async handle(request: any): Promise<HttpResponse> {
         const httpResponse = await this.controller.handle(request)
-        // if (httpResponse.statusCode === 500) {
-        //     await this.logRepository.logError(httpResponse.body)
-        // }
+        if (httpResponse.statusCode === 500) {
+            await this.logRepository.logError(httpResponse.body)
+        }
         return httpResponse
     }
 }
