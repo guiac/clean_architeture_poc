e0fa4a1c8caccc4a6b3c091743768c54c24c765c
guiac <gui.acassemiro@gmail.com>
2022-12-22 16:40:43 -0300

    :test_tube: Should return a token on sign success

--- tests/infra/cryptoaphy/jwt.adapter.spec.ts
+++ tests/infra/cryptoaphy/jwt.adapter.spec.ts
@@ -3,7 +3,7 @@ import { JwtAdapter } from '@/infra/cryptography'
 
 jest.mock('jsonwebtoken', () => ({
     async sign(): Promise<string> {
-        return 'any_value'
+        return 'any_token'
     },
 
     async verify(): Promise<string> {
@@ -13,11 +13,19 @@ jest.mock('jsonwebtoken', () => ({
 const secret = 'any_secret'
 const makeSut = (): JwtAdapter => { return new JwtAdapter(secret) }
 
-describe('encrypt()', () => {
-    test('Should call sign with correct values', async () => {
-        const sut = makeSut()
-        const signSpySpy = jest.spyOn(jwt, 'sign')
-        await sut.encrypt('any_id')
-        expect(signSpySpy).toHaveBeenCalledWith({ id: 'any_id' }, 'any_secret')
+describe('Jwt Adapter', () => {
+    describe('Jwt Adapter', () => {
+        test('Should call sign with correct values', async () => {
+            const sut = makeSut()
+            const signSpySpy = jest.spyOn(jwt, 'sign')
+            await sut.encrypt('any_id')
+            expect(signSpySpy).toHaveBeenCalledWith({ id: 'any_id' }, 'any_secret')
+        })
+
+        test('Should return a token on sign success', async () => {
+            const sut = makeSut()
+            const response = await sut.encrypt('any_id')
+            expect(response).toBe('any_token')
+        })
     })
 })
