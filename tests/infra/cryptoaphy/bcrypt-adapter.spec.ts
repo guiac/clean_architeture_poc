6a1b94e57722a5f67ff67461140dcabcbc3cd975
guiac <gui.acassemiro@gmail.com>
2022-12-22 10:55:12 -0300

    :test_tube: ensure Bcrypter throw if bcrypt throws

--- tests/infra/cryptoaphy/bcrypt-adapter.spec.ts
+++ tests/infra/cryptoaphy/bcrypt-adapter.spec.ts
@@ -11,6 +11,10 @@ jest.mock('bcrypt', () => ({
     }
 }))
 
+const throwError = (): never => {
+    throw new Error()
+}
+
 const salt = 12
 const makeSut = (): BCryptAdapter => {
     return new BCryptAdapter(salt)
@@ -29,6 +33,13 @@ describe('Bcrypt Adapter', () => {
             const sut = makeSut()
             const hash = await sut.hash('any_value')
             expect(hash).toBe('hash')
+        })
+
+        test('Should throw if hash throws', async () => {
+            const sut = makeSut()
+            jest.spyOn(bcrypt, 'hash').mockImplementationOnce(throwError)
+            const promise = sut.hash('any_value')
+            await expect(promise).rejects.toThrow()
         })
     }
     )
