8999c5ccbd1862b23ca5b521dccb2e5d44887661
guiac <gui.acassemiro@gmail.com>
2022-12-23 08:45:44 -0300

    :test_tube: Should call isEmail with correct values

---
+++ tests/infra/validators/email-validator-adapter.spec.ts
@@ -0,0 +1,21 @@
+import validator from 'validator'
+import { EmailValidatorAdapter } from '@/infra/validators'
+
+jest.mock('validator', () => ({
+    isEmail(email: string): boolean {
+        return true
+    }
+}))
+
+const makeSut = (): EmailValidatorAdapter => {
+    return new EmailValidatorAdapter()
+}
+
+describe('EmailValidatorAdapter', () => {
+    test('Should call isEmail with correct values', () => {
+        const sut = makeSut()
+        const isEmailSpy = jest.spyOn(validator, 'isEmail')
+        sut.isValid('gui.acassemiro@gmail.com')
+        expect(isEmailSpy).toHaveBeenCalledWith('gui.acassemiro@gmail.com')
+    })
+})
