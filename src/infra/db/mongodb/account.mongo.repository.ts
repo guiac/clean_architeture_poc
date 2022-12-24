6fb69ba016f09b887d1a72f400f5f8f371a31085
guiac <gui.acassemiro@gmail.com>
2022-12-24 17:50:35 -0300

    :sparkles: Should return an account on success

---
+++ src/infra/db/mongodb/account.mongo.repository.ts
@@ -0,0 +1,20 @@
+import { AddAccountRepository, CheckAccountByEmailRepository, LoadAccountByEmailRepository } from '@/data/protocols'
+import { LoadAccountByEmail } from '@/domain/usecases'
+import { AccountModel } from './models'
+
+export class AccountMongoRepository implements AddAccountRepository {
+    async save(data: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
+        const model = new AccountModel(data)
+        const result = await model.save()
+        return result.identification !== null
+    }
+
+    // async checkAccountByEmail(email: string): Promise<CheckAccountByEmailRepository.Result> {
+    //     const result = await AccountModel.findOne({ email })
+    //     return !!result
+    // }
+
+    // async loadAccountByEmail(email: string): Promise<LoadAccountByEmail.Result> {
+    //     return await AccountModel.findOne({ email })
+    // }
+}
