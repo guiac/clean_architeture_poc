096d68751b7c85dcd5eb54929290b98fda0e5ab1
guiac <gui.acassemiro@gmail.com>
2022-12-24 19:16:58 -0300

    :sparkles: account model

---
+++ src/infra/db/mongodb/models/account.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from 'mongoose'
+
+const AccountSchema = new Schema({
+    identification: {
+        type: String,
+        index: true,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    lastName: String,
+    birthDate: Date,
+    tellphone: String,
+    cellphone: String,
+    streetAddress: String,
+    numberAddress: String,
+    districtAddress: String,
+    cityAddress: String,
+    stateAddress: String
+})
+
+export const AccountModel = mongoose.model('AccountModel', AccountSchema)
