2b5608c6c0e861b6c86e29dbf891e1486daa86c5
guiac <gui.acassemiro@gmail.com>
2022-12-24 19:13:48 -0300

    :sparkles: mongoose setup

---
+++ src/infra/db/mongodb/mongo.helper.ts
@@ -0,0 +1,17 @@
+import { MongoClient } from 'mongodb'
+import mongoose from 'mongoose'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  uri: null as string,
+
+  async connect(uri: string): Promise<void> {
+    this.uri = uri
+    this.client = await mongoose.connect(uri)
+  },
+
+  async disconnect(): Promise<void> {
+    await this.client.close()
+    this.client = null
+  }
+}
