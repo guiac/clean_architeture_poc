c12b9565062d86dbec75c7e63edea049367937bb
guiac <gui.acassemiro@gmail.com>
2022-12-26 09:53:36 -0300

    :sparkles: Creating makeSignUpController

---
+++ src/main/factories/signup.controller.factory.ts
@@ -0,0 +1,10 @@
+import { SignUpController } from '@/presentation/controllers'
+import { Controller } from '@/presentation/protocols'
+import { makeSignUpValidation } from '.'
+import { makeDbAddAccount, makeDbAuthentication } from '@/main/usecases'
+import { makeLogControllerDecorator } from './decorators'
+
+export const makeSignUpController = (): Controller => {
+    const controller = new SignUpController(makeSignUpValidation(), makeDbAddAccount(), makeDbAuthentication())
+    return makeLogControllerDecorator(controller)
+}
