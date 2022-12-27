df751dc5340a184dc58d9966f4df0479588e8e00
guiac <gui.acassemiro@gmail.com>
2022-12-27 15:59:07 -0300

    :sparkles: Should return 400 if UpdateAccount return false

--- src/presentation/controllers/update.account.controller.ts
+++ src/presentation/controllers/update.account.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@/presentation/protocols/controller'
 import { Validation } from '../protocols/validation'
 import { badRequest, serverError, ok } from '../helpers/http.helper'
+import { UserNotFoundError } from '@/presentation/errors'
 import { UpdateAccount } from '@/domain/usecases'
 
 export class UpdateAccountController implements Controller {
@@ -14,6 +15,7 @@ export class UpdateAccountController implements Controller {
             const error = this.validation.validate(data)
             if (error) return badRequest(error)
             const accountResult = await this.updateAccount.handle(data)
+            if (!accountResult) return badRequest(new UserNotFoundError())
             return ok(accountResult)
         } catch (error) {
             return serverError(error)
