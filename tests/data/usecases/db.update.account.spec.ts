7561c55a33be64029cc08d966723728ee3e63285
guiac <gui.acassemiro@gmail.com>
2022-12-27 16:21:29 -0300

    :test_tube: Should call CheckAccountByIdRepositorySpy with correct values

---
+++ tests/data/usecases/db.update.account.spec.ts
@@ -0,0 +1,43 @@
+import { UpdateAccount } from '@/domain/usecases'
+import { DbUpdateAccount } from '@/data/usecases'
+import { CheckAccountByIdRepositorySpy } from '@/tests/data/mocks'
+
+const throwError = (): never => {
+    throw new Error()
+}
+
+const mockeRequest = (): UpdateAccount.Request => ({
+    identification: 'any_id',
+    name: 'name',
+    lastName: 'lastName',
+    birthDate: new Date(),
+    tellphone: 'tellphone',
+    cellphone: 'cellphone',
+    streetAddress: 'streetAddress',
+    numberAddress: 'numberAddress',
+    districtAddress: 'districtAddress',
+    cityAddress: 'cityAddress',
+    stateAddress: 'stateAddress'
+})
+
+type SutTypes = {
+    sut: DbUpdateAccount
+    checkAccountByIdRepositorySpy: CheckAccountByIdRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+    const checkAccountByIdRepositorySpy = new CheckAccountByIdRepositorySpy()
+    const sut = new DbUpdateAccount(checkAccountByIdRepositorySpy)
+    return {
+        checkAccountByIdRepositorySpy,
+        sut
+    }
+}
+
+describe('DbUpdateAccount Usecase', () => {
+    test('Should call CheckAccountByIdRepositorySpy with correct values', async () => {
+        const { sut, checkAccountByIdRepositorySpy } = makeSut()
+        await sut.handle(mockeRequest())
+        expect(checkAccountByIdRepositorySpy.params).toBe(mockeRequest().identification)
+    })
+})
