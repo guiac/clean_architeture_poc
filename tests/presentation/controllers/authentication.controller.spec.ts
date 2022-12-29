fb11900d87c41a46e5767b1ed3ca9c42360d86ab
guiac <gui.acassemiro@gmail.com>
2022-12-29 17:46:10 -0300

    :test_tube: Should return 400 if Validation fails

--- tests/presentation/controllers/authentication.controller.spec.ts
+++ tests/presentation/controllers/authentication.controller.spec.ts
@@ -27,4 +27,12 @@ describe('AuthenticationController', () => {
         await sut.handle(request)
         expect(validationSpy.input).toEqual(request)
     })
+
+    test('Should return 400 if Validation fails', async () => {
+        const { sut, validationSpy } = makeSut()
+        validationSpy.error = new Error()
+        const request = mockRequest()
+        const httpResponse = await sut.handle(request)
+        expect(httpResponse.statusCode).toBe(400)
+    })
 })
