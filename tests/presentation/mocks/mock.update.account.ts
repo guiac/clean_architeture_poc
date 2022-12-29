6266823a7d049eb0fa90d0a1a986f74b29628905
guiac <gui.acassemiro@gmail.com>
2022-12-29 16:51:04 -0300

    :construction: fix updateAccount return type

--- tests/presentation/mocks/mock.update.account.ts
+++ tests/presentation/mocks/mock.update.account.ts
@@ -2,7 +2,7 @@ import { UpdateAccount } from '@/domain/usecases'
 
 export class UpdateAccountSpy implements UpdateAccount {
     input: any
-    async handle(input: UpdateAccount.Request): Promise<UpdateAccount.Result> {
+    async handle(input: UpdateAccount.Request): Promise<UpdateAccount.Result | boolean> {
         this.input = input
         return this.input
     }
