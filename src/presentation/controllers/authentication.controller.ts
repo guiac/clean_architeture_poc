5c662ef30ef4c6899e6dfbe222a54977c1daafd1
guiac <gui.acassemiro@gmail.com>
2022-12-29 18:11:20 -0300

    :sparkles: Should call Authentication with correct values

--- src/presentation/controllers/authentication.controller.ts
+++ src/presentation/controllers/authentication.controller.ts
@@ -1,15 +1,17 @@
 import { Controller } from '@/presentation/protocols/controller'
-import { badRequest } from '../helpers'
+import { badRequest } from '@/presentation/helpers'
 import { Validation } from '../protocols/validation'
-
+import { Authentication } from '@/domain/usecases'
 export class AuthenticationController implements Controller {
     constructor(
-        private readonly validation: Validation
+        private readonly validation: Validation,
+        private readonly authentication: Authentication
     ) { }
 
     async handle(data: AuthenticationController.Request): Promise<any> {
         const error = this.validation.validate(data)
         if (error) return badRequest(error)
+        await this.authentication.handle(data)
     }
 }
 
