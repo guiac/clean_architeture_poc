91da8a663bfc955ee1e18d21e93f92928497b058
guiac <gui.acassemiro@gmail.com>
2022-12-30 23:42:45 -0300

    :sparkles: login route

--- src/main/routes/account.routes.ts
+++ src/main/routes/account.routes.ts
@@ -1,9 +1,10 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeSignUpController, makeUpdateAccountControler } from '@/main/factories'
+import { makeSignUpController, makeUpdateAccountControler, makeAuthenticationController } from '@/main/factories'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
     router.post('/signup', adaptRoute(makeSignUpController()))
+    router.post('/login', adaptRoute(makeAuthenticationController()))
     router.put('/account/update/:identification', adaptRoute(makeUpdateAccountControler()))
 }
