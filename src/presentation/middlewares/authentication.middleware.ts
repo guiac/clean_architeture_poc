e2e4750825dbd5ad73b38fd30d8c96dcb8820541
guiac <gui.acassemiro@gmail.com>
2023-01-03 18:54:21 -0300

    :sparkles: Should return 403 if no x-access-token exists in headers

--- src/presentation/middlewares/authentication.middleware.ts
+++ src/presentation/middlewares/authentication.middleware.ts
@@ -11,14 +11,17 @@ export class AuthenticationMiddleware implements Middleware {
 
     async handle(httpRequest: AuthenticationMiddleware.Request): Promise<any> {
         const { accessToken } = httpRequest
-        const account = await this.loadAccountByTokenRepository.load(accessToken, this.role)
-        if (!account) return forbidden(new AccessDeniedError())
+        if (accessToken) {
+            const account = await this.loadAccountByTokenRepository.load(accessToken, this.role)
+            if (!account) return forbidden(new AccessDeniedError())
+        }
+        return forbidden(new AccessDeniedError())
     }
 }
 
 export namespace AuthenticationMiddleware {
     export type Request = {
-        accessToken: string
+        accessToken?: string
     }
     export type Result = boolean
 }
