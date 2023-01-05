5b4f82aa3d5c09300d92a59b332be4aa6e6753e6
guiac <gui.acassemiro@gmail.com>
2023-01-05 07:16:20 -0300

    :sparkles: Should return null if Decrypter returns null

--- src/data/usecases/db.load.account.by.token.ts
+++ src/data/usecases/db.load.account.by.token.ts
@@ -4,6 +4,7 @@ import { Decrypter } from '../protocols'
 export class DbLoadAccountByToken implements LoadAccountByToken {
     constructor(private readonly decrypter: Decrypter) { }
     async handle(data: LoadAccountByToken.Request): Promise<any> {
-        await this.decrypter.decrypt(data.accessToken)
+        const token = await this.decrypter.decrypt(data.accessToken)
+        if (!token) return token
     }
 }
--- src/data/usecases/index.ts
+++ src/data/usecases/index.ts
@@ -1,3 +1,4 @@
 export * from './db.add.account'
 export * from './db.update.account'
 export * from './db.authentication'
+export * from './db.load.account.by.token'
