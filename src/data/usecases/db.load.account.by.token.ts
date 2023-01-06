3368126635008413e3e17547ff4a6f4e3487fc0a
guiac <gui.acassemiro@gmail.com>
2023-01-05 21:15:04 -0300

    :sparkles: Should return null if LoadAccountByTokenRepository return null

--- src/data/usecases/db.load.account.by.token.ts
+++ src/data/usecases/db.load.account.by.token.ts
@@ -1,5 +1,6 @@
 import { LoadAccountByToken } from '@/domain/usecases'
 import { Decrypter, LoadAccountByTokenRepository } from '@/data/protocols'
+
 export class DbLoadAccountByToken implements LoadAccountByToken {
     constructor(
         private readonly decrypter: Decrypter,
@@ -13,6 +14,7 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
         } catch (error) {
             return null
         }
-        await this.loadAccountByTokenRepository.load(data)
+        const account = await this.loadAccountByTokenRepository.load(data)
+        if (!account) return account
     }
 }
