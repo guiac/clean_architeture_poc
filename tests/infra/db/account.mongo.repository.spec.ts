dbffae3eb298698905704ab25c1bd8cf05adbae9
guiac <gui.acassemiro@gmail.com>
2023-01-07 10:53:50 -0300

    :test_tube: Should return null on loadByToken with invalid role

--- tests/infra/db/account.mongo.repository.spec.ts
+++ tests/infra/db/account.mongo.repository.spec.ts
@@ -25,6 +25,24 @@ const addAccountParams = (): AddAccountRepository.Params => ({
     accessToken: 'accessToken'
 })
 
+const addAccountParamsWithRoleAdmin = (): AddAccountRepository.Params => ({
+    email: 'adminl@gmail.com',
+    password: 'any_password',
+    identification: 'admin_identification',
+    name: 'name',
+    lastName: 'lastName',
+    birthDate: new Date(),
+    tellphone: 'tellphone',
+    cellphone: 'cellphone',
+    streetAddress: 'streetAddress',
+    numberAddress: 'numberAddress',
+    districtAddress: 'districtAddress',
+    cityAddress: 'cityAddress',
+    stateAddress: 'stateAddress',
+    accessToken: 'accessToken',
+    role: 'admin'
+})
+
 describe('AccountMongoRepository', () => {
     beforeAll(async () => await MongoTestDbHelper.connect())
     afterEach(async () => await MongoTestDbHelper.clearDatabase())
@@ -123,13 +141,23 @@ describe('AccountMongoRepository', () => {
             expect(account).toBeDefined()
         })
 
-        test('Should return an account on loadByToken with role', async () => {
+        test('Should return an account on loadByToken with admin role', async () => {
             const sut = makeSut()
             const request = addAccountParams()
-            await sut.save({ ...request, role: 'admin' })
-            const account = await sut.load({ accessToken: request.accessToken, role: 'admin' })
+            const requestAdmin = addAccountParamsWithRoleAdmin()
+            await sut.save(requestAdmin)
+            await sut.save(request)
+            const account = await sut.load({ accessToken: requestAdmin.accessToken, role: 'admin' })
             expect(account).toBeDefined()
             expect(account.identification).toBeTruthy()
+        })
+
+        test('Should return null on loadByToken with invalid role', async () => {
+            const sut = makeSut()
+            const request = addAccountParams()
+            await sut.save(request)
+            const account = await sut.load({ accessToken: request.accessToken, role: 'admin' })
+            expect(account).toBeNull()
         })
     })
 })
