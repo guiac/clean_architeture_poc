63db091334a49a839e0a2a12185aa4b467498ffb
guiac <gui.acassemiro@gmail.com>
2023-01-08 09:17:54 -0300

    :sparkles: Adding middleware in the routes

--- src/main/routes/account.routes.ts
+++ src/main/routes/account.routes.ts
@@ -2,9 +2,10 @@ import { adaptRoute } from '@/main/adapters'
 import { makeSignUpController, makeUpdateAccountControler, makeAuthenticationController } from '@/main/factories'
 
 import { Router } from 'express'
+import { auth } from '@/main/middlewares'
 
 export default (router: Router): void => {
     router.post('/signup', adaptRoute(makeSignUpController()))
     router.post('/login', adaptRoute(makeAuthenticationController()))
-    router.put('/account/update/:identification', adaptRoute(makeUpdateAccountControler()))
+    router.put('/account/update/:identification', auth, adaptRoute(makeUpdateAccountControler()))
 }
